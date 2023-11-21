caf782ed9b0dfdeb0a7c83b458cc4ecfcd51a7c9
SankabathulaDurgabhavani <119723748+SankabathulaDurgabhavani@users.noreply.github.com>
2023-11-21 15:19:51 +0530

    #include <stdio.h> int main() {     int n1, n2, i, gcd;
        scanf("%d %d", &n1, &n2);
    
        for(i=1; i <= n1 && i <= n2; ++i)
        {
            if(n1%i==0 && n2%i==0)
                gcd = i;
        }
    
        printf("%d",gcd);
    
    return 0;
    }

--- Greatest_Common_Divisor.c
+++ Greatest_Common_Divisor.c
@@ -1,14 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n,m,i,gcd=0;
-    scanf("%d%d",&n,&m);
-    for(i=1;i<=n&&i<=m;i++)
+    int n1, n2, i, gcd;
+
+    scanf("%d %d", &n1, &n2);
+
+    for(i=1; i <= n1 && i <= n2; ++i)
     {
-       if(n%i==0 && m%i==0)
-       {
-           gcd=i;
-       }
+        if(n1%i==0 && n2%i==0)
+            gcd = i;
     }
+
     printf("%d",gcd);
+
+return 0;
 }
