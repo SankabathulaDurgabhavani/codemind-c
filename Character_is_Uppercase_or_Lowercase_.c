82461aab1d08b55b90bb8f7651bd69ce05832e1e
SankabathulaDurgabhavani <119723748+SankabathulaDurgabhavani@users.noreply.github.com>
2023-11-22 15:33:30 +0530

    #include<stdio.h> int main() {     char s;     scanf("%c",&s);     if(s>='A' && s<='Z')     {         printf("uppercase alphabet");     }     else if(s>='a' && s<='z')     {         printf("lowercase alphabet");     }     else     {         printf("not an alphabet");     } }

---
+++ Character_is_Uppercase_or_Lowercase_.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char s;
+    scanf("%c",&s);
+    if(s>='A' && s<='Z')
+    {
+        printf("uppercase alphabet");
+    }
+    else if(s>='a' && s<='z')
+    {
+        printf("lowercase alphabet");
+    }
+    else
+    {
+        printf("not an alphabet");
+    }
+}
