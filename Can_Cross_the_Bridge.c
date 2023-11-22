774b41b3713e9d5ac2b4b5de7775593b0ce9e495
SankabathulaDurgabhavani <119723748+SankabathulaDurgabhavani@users.noreply.github.com>
2023-11-22 14:16:17 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     int s=(c-b)/a;     printf("%d",s); }

--- Can_Cross_the_Bridge.c
+++ Can_Cross_the_Bridge.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int x,y,z;
-    scanf("%d%d%d",&x,&y,&z);
-    int a=(z-y);
-    int b=(a/x);
-    printf("%d",b);
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    int s=(c-b)/a;
+    printf("%d",s);
 }
